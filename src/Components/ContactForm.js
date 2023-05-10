3878eb7e53a30d54f7d7036325f40168f3d507e7
abhiramkrishnansree <abhiramkrishnanrajan@gmail.com>
2023-05-10 21:07:32 +0530

    your commit message

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,21 +1,36 @@
 import React from 'react'
+import { useRef } from 'react';
 import './ContactForm.css';
+import emailjs from '@emailjs/browser';
 const ContactForm = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_d3edc8u', 'template_00oxg9s', form.current, 'jDd1i8aP_dNxx_tQ_')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
   return (
     <div className='contact'>
-    <form>
+    <form ref={form} onSubmit={sendEmail}>
     <h2>Contact Me</h2>
       <div className='pun'>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" placeholder='Enter your Name'/>
+        <input type="text" id="name" name="user_name" placeholder='Enter your FullName' required/>
       </div>
       <div className='pun'>
         <label htmlFor="email">Email:</label>
-        <input placeholder='Eg. example@gmail.com'/>
+        <input name="user_email" placeholder='Eg. example@gmail.com' required/>
       </div>
       <div className='pun'>
         <label htmlFor="message">Message:</label>
-        <textarea maxlength="3000" placeholder='Please enter your comments..'/>
+        <textarea name='message' maxlength="3000" placeholder='Please enter your comments..'/>
       </div>
       <button type="submit">Send Message</button>
     </form>
